d52caf35053dae6f9f0d04be654bd762ec618558
nerdyallowance <118093630+nerdyallowance@users.noreply.github.com>
2026-06-13 16:59:15 +0530

    Add files via upload

---
+++ day-17/Q67.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n1, n2, i, j, arr1[100], arr2[100];
+
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+    printf("Enter %d elements:\n", n1);
+    for (i = 0; i < n1; i++) scanf("%d", &arr1[i]);
+
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+    printf("Enter %d elements:\n", n2);
+    for (i = 0; i < n2; i++) scanf("%d", &arr2[i]);
+
+    printf("Intersection of arrays:\n");
+    for (i = 0; i < n1; i++) {
+        for (j = 0; j < n2; j++) {
+            if (arr1[i] == arr2[j]) {
+                printf("%d ", arr1[i]);
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
+
