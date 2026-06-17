de2a2c672b0d6ab446c3d135cfe3d8b7a74a7266
nerdyallowance <118093630+nerdyallowance@users.noreply.github.com>
2026-06-17 23:54:07 +0530

    Add files via upload

---
+++ day-20/Q80.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Program Q80 placeholder generated for Summer Vacation Assignment.\n");
+    printf("Implement logic according to question Q80.\n");
+    return 0;
+}
