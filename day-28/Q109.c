047787cf35b0a142217ab40d34081c529848a3f3
nerdyallowance <118093630+nerdyallowance@users.noreply.github.com>
2026-06-26 22:16:45 +0530

    Add files via upload

---
+++ day-28/Q109.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Program Q109 placeholder generated for Summer Vacation Assignment.\n");
+    printf("Implement logic according to question Q109.\n");
+    return 0;
+}
